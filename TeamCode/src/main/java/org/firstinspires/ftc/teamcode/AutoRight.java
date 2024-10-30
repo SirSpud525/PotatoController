782eb699b1de86af31e8b1e2d7afd36b00b00025
Dino5341 <pranav.soma@icloud.com>
2024-10-30 11:24:06 -0400

    Update AutoRight.java to be similar to AutoLeft
    It should work hopefully. Its also similar to AutoLeft

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoRight.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoRight.java
@@ -2,8 +2,18 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
-public class AutoRight {
-}
+@Autonomous(name = "AutoRight", group = "Right")
+public class AutoRight extends LinearOpMode {
+
+  PotatoRobot robot = new PotatoRobot();
+
+  public void runOpMode() throws InterruptedException {
+    robot.init(hardwareMap);
+    waitForStart();
+    robot.driveToInches(6, telemetry);
+    robot.strafe(24);
+    stop();
+  }
 
 
 // Move forward 6 inches, strafe 24 inches
