8f8f449585c55614fb91bb5c02bbf121048d032d
Dino5341 <pranav.soma@icloud.com>
2024-10-30 11:15:51 -0400

    Update and rename AutoRedRight.java to AutoRight.java
    Importing stuff!

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoRight.java
@@ -0,0 +1,9 @@
+package org.firstinspires.ftc.teamcode;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+public class AutoRight {
+}
+
+
+// Move forward 6 inches, strafe 24 inches
