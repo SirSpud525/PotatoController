3b0f7432b414818f8db93e950e1f472a62480f58
ps-coding <95548088+ps-coding@users.noreply.github.com>
2024-11-01 18:32:10 -0400

    organization fix for AutoLeft - still remember to change init to 1.0 for claw

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoLeft.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoLeft.java
@@ -4,16 +4,16 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 @Autonomous(name = "AutoLeft", group = "Left")
-public class AutoRedLeft extends LinearOpMode {
+public class AutoLeft extends LinearOpMode {
 
-  PotatoRobot robot = new PotatoRobot();
+    PotatoRobot robot = new PotatoRobot();
 
-  public void runOpMode() throws InterruptedException {
-    robot.init(hardwareMap);
-    waitForStart();
-    robot.driveToInches(30, telemetry);
-    robot.strafe(4600);
-    stop();
-  }
+    public void runOpMode() throws InterruptedException {
+        robot.init(hardwareMap);
+        waitForStart();
+        robot.driveToInches(30, telemetry);
+        robot.strafe(4600);
+        stop();
+    }
 
 }
